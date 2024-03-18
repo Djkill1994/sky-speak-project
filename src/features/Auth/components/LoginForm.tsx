3a72ce9fd47d1f1bd78759ea8f7e9c07fc892101
Vlad <84898137+Djkill1994@users.noreply.github.com>
2024-03-19 02:30:23 +0300

    Added login form

--- src/features/Auth/components/LoginForm.tsx
+++ src/features/Auth/components/LoginForm.tsx
@@ -1,13 +1,77 @@
-import { Box, Button } from "@mui/material";
-import { useNavigate } from "@tanstack/react-router";
+import { Box, Grid, TextField } from "@mui/material";
+import { EMAIL_REGEX } from "~/constans/regex.ts";
+import { FormInputPassword } from "~/libs/ui-kit/FormInputPassword.tsx";
+import { LoadingButton } from "@mui/lab";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { supabase } from "~/api/supavaseClient.ts";
+
+export interface ILoginForm {
+  email: string;
+  password: string;
+}
 
 export const LoginForm = () => {
-  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginForm>();
+  // const navigate = useNavigate();
+
+  const onSubmit: SubmitHandler<ILoginForm> = async (data) => {
+    await supabase.auth.signInWithPassword({
+      email: data.email,
+      password: data.password,
+    });
+  };
+
   return (
-    <Box>
-      LOGIN
-      <Button onClick={() => navigate({ to: "/" })}>Back</Button>
-      <Button onClick={() => navigate({ to: "/user" })}>User</Button>
+    <Box
+      margin="100px auto"
+      width="300px"
+      component="form"
+      onSubmit={handleSubmit(onSubmit)}
+      noValidate
+    >
+      <Grid
+        container
+        spacing={1}
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Grid item xs={12} width="100%">
+          <TextField
+            {...register("email", { required: true, pattern: EMAIL_REGEX })}
+            error={!!errors.email}
+            helperText={!!errors.email && "email"}
+            size="small"
+            autoComplete="email"
+            label={"email"}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} width="100%">
+          <FormInputPassword
+            id="password"
+            error={errors.password && "password"}
+            label={"password"}
+            inputProps={register("password", {
+              required: true,
+            })}
+          />
+        </Grid>
+        <Grid item xs={12} width="100%">
+          <LoadingButton
+            type="submit"
+            variant="contained"
+            sx={{ mt: 3 }}
+            fullWidth
+          >
+            {"signUp"}
+          </LoadingButton>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
